cf58cbdf6e22b1810df7a0e5a1195013d57cb06a
hhrnjic1 <148076339+hhrnjic1@users.noreply.github.com>
2023-10-29 13:25:58 +0100

    ...

---
+++ src/main/java/org/example/Banka.java
@@ -0,0 +1,26 @@
+package org.example
+java.uti.List
+public class Banka{
+     private long brojRacuna;
+     Banka(){
+          brojRacuna = 0;
+     }
+     public Korisnik kreirajNovogKorisnika(string ime, string prezime){
+          Korisnik korisnik(ime,prezime);
+          return korisnik;
+     }
+     public Uposlenik kreirajNovogUposlenika(string ime, string prezime){
+          Uposelnik uposlenik(ime,prezime);
+          return uposlenik;
+     }
+     public Racun kreirajRacunZaKorisnika(Korisnik korisnik) {
+          Racun racun (0, korisnik);
+          return racun;
+     }
+     public List<Korisnik> korisnici(){
+          return 0;
+     }
+     public List<Uposlenik> uposlenici(){
+          return 0;
+     }
+}
