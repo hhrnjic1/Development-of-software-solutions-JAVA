f5577b4d623d87894e61f1d253e2462a77c90939
hhrnjic1 <148076339+hhrnjic1@users.noreply.github.com>
2023-11-01 23:49:30 +0100

    ...

--- ETF_Telfonski_Imenik/src/main/java/etf/unsa/ba/rpr/Imenik.java
+++ ETF_Telfonski_Imenik/src/main/java/etf/unsa/ba/rpr/Imenik.java
@@ -1,5 +1,33 @@
 package etf.unsa.ba.rpr;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Imenik {
-        
+    HashMap<String,TelefonskiBroj> imenik;
+
+    public void dodaj(String ime , TelefonskiBroj broj){
+        imenik.put(ime,broj);
+    }
+    public String dajBroj(String ime){
+        TelefonskiBroj a = imenik.get(ime);
+        return a.ispisi();
+    }
+    public String dajIme(TelefonskiBroj broj){
+        for(Map.Entry<String,TelefonskiBroj> entry : imenik.entrySet()){
+            if(entry.getValue().equals(broj)){
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+    public String naSlovo(char s){
+        String spisak = "";
+        Object br = 1;
+        for(Map.Entry<String,TelefonskiBroj> pretrazivac : imenik.entrySet()){
+            if(pretrazivac.getKey().indexOf(s) == 0){
+                spisak += br + ". " + pretrazivac.getKey() + " - " + pretrazivac.getValue().ispisi()+"\n";
+            }
+        }
+        return spisak;
+    }
 }
