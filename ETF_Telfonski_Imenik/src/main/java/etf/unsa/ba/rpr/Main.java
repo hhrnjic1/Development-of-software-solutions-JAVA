fc94e510a3e354cb7e4acee8be2164b6dd95c3b2
hhrnjic1 <148076339+hhrnjic1@users.noreply.github.com>
2023-11-01 18:39:41 +0100

    Delete ETF_Telfonski_Imenik directory

--- ETF_Telfonski_Imenik/src/main/java/etf/unsa/ba/rpr/Main.java
+++
@@ -1,8 +0,0 @@
-package etf.unsa.ba.rpr;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- ETF_Telfonski_Imenik/src/main/java/etf/unsa/ba/rpr/TelefonskiBroj.java
+++
@@ -1,6 +0,0 @@
-package etf.unsa.ba.rpr;
-
-public interface TelefonskiBroj {
-    public String ispisi();
-    int hasCode();
-}
